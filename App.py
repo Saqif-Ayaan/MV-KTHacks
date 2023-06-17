05be86c22fcadbf74878fb57705e4d2fea45f973
Kazu-Pang <72171546+Kazu-Pang@users.noreply.github.com>
2023-06-17 13:39:17 -0700

    outline

--- App.py
+++ App.py
@@ -4,8 +4,14 @@ def initApp():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'password'
 
-    from Connector import connector
-    
-    app.register_blueprint(connector, url_prefix = '/')
+    from Connector import st
+    from Connector import qz
+    from Connector import rd
 
+    app.register_blueprint(st, url_prefix = '/')
+    app.register_blueprint(rd, url_prefix = '/')
+    app.register_blueprint(qz, url_prefix = '/')
+    
     return app
+
+  
--- Connector.py
+++ Connector.py
@@ -1,7 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import os
 
-connector = Blueprint('connector', __name__)
+st = Blueprint('start', __name__)
+qz = Blueprint('quiz', __name__)
+rd = Blueprint('riskDetection', __name__)
 
-@connector.route('/')
+
+@st.route('/')
 def home():
     return render_template("Start.html")
+
+@qz.route('/quiz')
+def assessment():
+    return render_template("Quiz.html")
+
+@rd.route('/riskDetection', methods = ["POST", "GET"])
+def det():
+    if request.method == "POST":
+        file = request.files["file"]
+        file.save(os.path.join("Data", file.filename))
+    return render_template("Detection.html")
+
+
+
+
+
+
+
+
